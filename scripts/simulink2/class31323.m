cabe6509fec0782a1acfaa9b401592f7c2116dbb
Juan Felipe R <42847785+Juferoga@users.noreply.github.com>
2023-03-13 09:59:26 -0500

    Create

---
+++ scripts/simulink2/class31323.m
@@ -0,0 +1,16 @@
+clc; clear; close all;
+A=5;
+
+Ki=1;Kp=1;Kd=1;
+
+num=[0.75];
+den=[2 1];
+Gs=tf(num,den);
+step(Gs*A)
+hold on; grid on
+
+for Ki=0.5:0.5:5
+    Cs = tf([Ki],[1 0]);
+    Glc=feedback(series(Gs,Cs),1);
+    step(Glc*A)
+end
